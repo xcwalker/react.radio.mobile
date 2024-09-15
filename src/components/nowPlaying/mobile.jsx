3d8a5be85d20c1a206395ede0b1cb64fb05b4ec2
Xander Walker <xander@xcw.one>
2024-09-15 17:58:11 +0100

    Actual Bug Fix

--- src/components/nowPlaying/mobile.jsx
+++ src/components/nowPlaying/mobile.jsx
@@ -6,14 +6,14 @@ export default function MobileNowPlaying(props) {
   const [params, setParams] = useSearchParams();
   return (
     <div className="item" id="live">
-      {props?.nowPlaying.art && (
+      {props?.nowPlaying?.art && (
         <CrossfadeImage
-          src={props.nowPlaying.art}
+          src={props?.nowPlaying?.art}
           alt={
             "The artwork of " +
-            props.nowPlaying.title +
+            props?.nowPlaying?.title +
             " by " +
-            props.nowPlaying.artists
+            props?.nowPlaying?.artists
           }
           containerClass="background"
           style={{ width: "100%", height: "100%" }}
@@ -26,14 +26,14 @@ export default function MobileNowPlaying(props) {
       </div>
       {/* No Data */}
       {!(
-        props?.nowPlaying.title ||
-        props?.nowPlaying.artists ||
-        props?.nowPlaying.art
+        props?.nowPlaying?.title ||
+        props?.nowPlaying?.artists ||
+        props?.nowPlaying?.art
       ) && <span className="ident">{props.station}</span>}
       {/* Data */}
-      {(props?.nowPlaying.title ||
-        props?.nowPlaying.artists ||
-        props?.nowPlaying.art) && (
+      {(props?.nowPlaying?.title ||
+        props?.nowPlaying?.artists ||
+        props?.nowPlaying?.art) && (
         <div className="info">
           <button
             onClick={(e) => {
@@ -43,9 +43,9 @@ export default function MobileNowPlaying(props) {
                 : setParams({ artView: true });
             }}
           >
-            {props.nowPlaying.art && (
+            {props?.nowPlaying?.art && (
               <CrossfadeImage
-                src={props.nowPlaying.art}
+                src={props?.nowPlaying?.art}
                 alt={
                   "The artwork of " +
                   props.nowPlaying.title +
@@ -56,10 +56,10 @@ export default function MobileNowPlaying(props) {
               />
             )}
           </button>
-          {(props.nowPlaying.title || props.nowPlaying.artists) && (
+          {(props?.nowPlaying?.title || props?.nowPlaying?.artists) && (
             <div className="text">
-              <span className="title">{props.nowPlaying.title}</span>
-              <span className="subTitle">{props.nowPlaying.artists}</span>
+              <span className="title">{props?.nowPlaying?.title}</span>
+              <span className="subTitle">{props?.nowPlaying?.artists}</span>
             </div>
           )}
         </div>
