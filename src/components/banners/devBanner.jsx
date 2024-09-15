93ad9d2b7e8b8c310375bc40a45e447bf55c77e4
Xander Walker <xander.walker@xcw.one>
2024-09-15 18:05:27 +0100

    Bug Fix

--- src/components/banners/devBanner.jsx
+++ src/components/banners/devBanner.jsx
@@ -1,7 +1,7 @@
 export default function DevBanner(props) {
   return (
     <>
-      {process.env.REACT_APP_IS_DEVELOPMENT_BUILD && (
+      {process.env.REACT_APP_IS_DEVELOPMENT_BUILD === "true" && (
         <div
           className={props.css.banner}
           style={{ "--banner-color": "#ff8f00" }}
