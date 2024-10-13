26a21f2439402bba86c7958c80b60856ab67d288
Xander Walker <xander.walker@xcw.one>
2024-10-13 22:20:36 +0100

    DevBanner Changes

--- src/components/banners/devBanner.jsx
+++ src/components/banners/devBanner.jsx
@@ -1,9 +1,11 @@
+import css from "../../style/components/banner/developmentBanner.module.css"
+
 export default function DevBanner(props) {
   return (
     <>
       {process.env.REACT_APP_IS_DEVELOPMENT_BUILD === "true" && (
         <div
-          className={props.css.banner}
+          className={props.css.banner + " " + css.developmentBanner}
           style={{ "--banner-color": "#ff8f00" }}
         >
           Development Build
