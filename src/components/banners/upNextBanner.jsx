3b60c90cb5649d15e1db0043f6abc7e9c2d7b10a
Xander <xander@xcw.one>
2024-10-15 16:23:47 +0100

    Debug Fix

--- src/components/banners/upNextBanner.jsx
+++ src/components/banners/upNextBanner.jsx
@@ -6,7 +6,7 @@ export default function UpNextBanner(props) {
         props.djNext.details &&
         props.djNext.displayname &&
         (props.date.getMinutes() >= 50 ||
-          process.env.REACT_APP_IS_DEBUG_ALL_BANNERS !== "false") && (
+          process.env.REACT_APP_IS_DEBUG_ALL_BANNERS === "true") && (
           <div className={props.css.banner} style={{ "--banner-color": "red" }}>
             Up next • {props.djNext.details.replace(".", "")} •{" "}
             {props.djNext.displayname}
