4cd64241a5fbff9ad47f8e7ed0bb7da80fd4b0da
Xander Walker <xander@xcw.one>
2025-12-06 13:54:23 +0000

    Settings Changes

--- src/components/views/settingsView.jsx
+++ src/components/views/settingsView.jsx
@@ -1,6 +1,8 @@
 import { useAtom } from "jotai";
 import { settingsAtom } from "../../App";
 import "../../style/pages/settings.css";
+import "../../style/components/settings/settingWrapper.css";
+import "../../style/components/settings/navigationPosition.css";
 
 export default function SettingsView(props) {
   const [settings, setSettings] = useAtom(settingsAtom);
@@ -17,111 +19,131 @@ export default function SettingsView(props) {
     <section id="settings">
       <div className="container">
         <h1 className="mainTransition">Settings</h1>
-        <div className="setting mainTransition">
-          <span>Force Navigation Bar Position</span>
-          <div className="navigationSelection">
-            <div />
-            <div
-              className={
-                "top indicator " +
-                ((settings.forceNavigationBarStyle === "TopLeft" ||
-                  settings.forceNavigationBarStyle === "Above" ||
-                  settings.forceNavigationBarStyle === "TopRight") &&
-                  "active")
-              }
-            />
-            <div />
-            <div
-              className={
-                "left indicator " +
-                ((settings.forceNavigationBarStyle === "TopLeft" ||
-                  settings.forceNavigationBarStyle === "Left" ||
-                  settings.forceNavigationBarStyle === "DefaultLeft") &&
-                  "active")
-              }
-            />
-            <div className="center">
-              <button
-                onClick={() => {
-                  setSetting("forceNavigationBarStyle", "TopLeft");
-                }}
-              >
-                {/* Top & Left */}
-              </button>
-              <button
-                onClick={() => {
-                  setSetting("forceNavigationBarStyle", "Above");
-                }}
-              >
-                {/* Above */}
-              </button>
-              <button
-                onClick={() => {
-                  setSetting("forceNavigationBarStyle", "TopRight");
-                }}
-              >
-                {/* Top & Right */}
-              </button>
-              <button
-                onClick={() => {
-                  setSetting("forceNavigationBarStyle", "Left");
-                }}
-              >
-                {/* Left */}
-              </button>
-              <div />
-              <button
-                onClick={() => {
-                  setSetting("forceNavigationBarStyle", "Right");
-                }}
-              >
-                {/* Right */}
-              </button>
-              <button
-                onClick={() => {
-                  setSetting("forceNavigationBarStyle", "DefaultLeft");
-                }}
-              >
-                {/* Bottom & Left */}
-              </button>
-              <button
-                onClick={() => {
-                  setSetting("forceNavigationBarStyle", "Below");
-                }}
-              >
-                {/* Below */}
-              </button>
-              <button
-                onClick={() => {
-                  setSetting("forceNavigationBarStyle", undefined);
-                }}
-              >
-                {/* Bottom & Right */}
-              </button>
-            </div>
-            <div
-              className={
-                "right indicator " +
-                ((settings.forceNavigationBarStyle === "TopRight" ||
-                  settings.forceNavigationBarStyle === "Right" ||
-                  settings.forceNavigationBarStyle === undefined) &&
-                  "active")
-              }
-            />
-            <div />
-            <div
-              className={
-                "bottom indicator " +
-                ((settings.forceNavigationBarStyle === "DefaultLeft" ||
-                  settings.forceNavigationBarStyle === "Below" ||
-                  settings.forceNavigationBarStyle === undefined) &&
-                  "active")
-              }
-            />
-            <div />
-          </div>
-        </div>
+        <NavigationPosition settings={settings} setSetting={setSetting} />
       </div>
     </section>
+  );
+}
+
+function SettingWrapper(props) {
+  const { id, title, children } = props;
+  return (
+    <section id={id} className="settingWrapper mainTransition">
+      <h2>{title}</h2>
+      {children}
+    </section>
+  );
+}
+
+function NavigationPosition(props) {
+  const { settings, setSetting } = props;
+
+  return (
+    <SettingWrapper
+      id="navigationPosition"
+      title="Force Navigation Bar Position"
+    >
+      <div className="navigationSelection">
+        <div />
+        <div
+          className={
+            "top indicator " +
+            ((settings.forceNavigationBarStyle === "TopLeft" ||
+              settings.forceNavigationBarStyle === "Above" ||
+              settings.forceNavigationBarStyle === "TopRight") &&
+              "active")
+          }
+        />
+        <div />
+        <div
+          className={
+            "left indicator " +
+            ((settings.forceNavigationBarStyle === "TopLeft" ||
+              settings.forceNavigationBarStyle === "Left" ||
+              settings.forceNavigationBarStyle === "DefaultLeft") &&
+              "active")
+          }
+        />
+        <div className="center">
+          <button
+            onClick={() => {
+              setSetting("forceNavigationBarStyle", "TopLeft");
+            }}
+          >
+            {/* Top & Left */}
+          </button>
+          <button
+            onClick={() => {
+              setSetting("forceNavigationBarStyle", "Above");
+            }}
+          >
+            {/* Above */}
+          </button>
+          <button
+            onClick={() => {
+              setSetting("forceNavigationBarStyle", "TopRight");
+            }}
+          >
+            {/* Top & Right */}
+          </button>
+          <button
+            onClick={() => {
+              setSetting("forceNavigationBarStyle", "Left");
+            }}
+          >
+            {/* Left */}
+          </button>
+          <div />
+          <button
+            onClick={() => {
+              setSetting("forceNavigationBarStyle", "Right");
+            }}
+          >
+            {/* Right */}
+          </button>
+          <button
+            onClick={() => {
+              setSetting("forceNavigationBarStyle", "DefaultLeft");
+            }}
+          >
+            {/* Bottom & Left */}
+          </button>
+          <button
+            onClick={() => {
+              setSetting("forceNavigationBarStyle", "Below");
+            }}
+          >
+            {/* Below */}
+          </button>
+          <button
+            onClick={() => {
+              setSetting("forceNavigationBarStyle", undefined);
+            }}
+          >
+            {/* Bottom & Right */}
+          </button>
+        </div>
+        <div
+          className={
+            "right indicator " +
+            ((settings.forceNavigationBarStyle === "TopRight" ||
+              settings.forceNavigationBarStyle === "Right" ||
+              settings.forceNavigationBarStyle === undefined) &&
+              "active")
+          }
+        />
+        <div />
+        <div
+          className={
+            "bottom indicator " +
+            ((settings.forceNavigationBarStyle === "DefaultLeft" ||
+              settings.forceNavigationBarStyle === "Below" ||
+              settings.forceNavigationBarStyle === undefined) &&
+              "active")
+          }
+        />
+        <div />
+      </div>
+    </SettingWrapper>
   );
 }
